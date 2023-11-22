b3170b0959771f04a3baa8b1cb3255ae65588555
Jingshao Chen <jingshaochen@gmail.com>
2023-11-22 01:49:50 -0600

    updated to svelte-4

--- svelte.config.js
+++ svelte.config.js
@@ -1,16 +1,16 @@
-import preprocess from 'svelte-preprocess'
 import adapter from '@sveltejs/adapter-static'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   // Consult https://github.com/sveltejs/svelte-preprocess
   // for more information about preprocessors
-  preprocess: preprocess(),
+  // preprocess: preprocess(),
 
   kit: {
     // hydrate the <div id="svelte"> element in src/app.html
+
     adapter: adapter({
-      fallback: 'index.html',
+      fallback: 'index.html', // may differ from host to host
     }),
   },
 }
